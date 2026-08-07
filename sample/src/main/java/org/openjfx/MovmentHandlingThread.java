38eb9e1c9d7ff175e3e729ca8b3a9f13017f2fbb
marcobompani07 <marco.bompani.07@gmail.com>
2026-08-07 19:13:32 +0200

    finisched MovmentHandlingThread

--- sample/src/main/java/org/openjfx/MovmentHandlingThread.java
+++ sample/src/main/java/org/openjfx/MovmentHandlingThread.java
@@ -1,15 +1,38 @@
 package org.openjfx;
 
-public class MovmentHandlingThread extends Thread{
-    @Override
-    public MovmentHandlingThread(Runnable update){
+import javafx.application.Platform;
 
+public class MovmentHandlingThread extends Thread{  
+    private Runnable update;
+    private ThreadSafeUpdateQueueCounter counter;
+    private boolean stop;
+    public MovmentHandlingThread(Runnable update,ThreadSafeUpdateQueueCounter counter){
+        this.update=update;
+        this.counter=counter;
+        stop=false;
     }
+    @Override
     public void start(){
-
+        while (!stop){
+            long startTime=System.currentTimeMillis();
+            if (counter.getCounter()<2){
+                Platform.runLater(update);
+                counter.increaseCounter();
+            }
+            long sleeptime= 10-(System.currentTimeMillis()-startTime);
+            try{
+                if (sleeptime>0){
+                    Thread.sleep(sleeptime);
+                }
+            }catch(InterruptedException e){
+                e.printStackTrace();
+            }
+            
+            
+        }
     }
 
     public void Stop(){
-
+        stop=true;
     }
 }
