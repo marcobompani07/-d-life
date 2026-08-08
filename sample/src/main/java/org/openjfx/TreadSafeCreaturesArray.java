14209e7ff228327705272969343c74f6d3dc348c
marcobompani07 <marco.bompani.07@gmail.com>
2026-08-08 17:30:39 +0200

    compleated thread safe array

--- sample/src/main/java/org/openjfx/TreadSafeCreaturesArray.java
+++ sample/src/main/java/org/openjfx/TreadSafeCreaturesArray.java
@@ -1,42 +1,26 @@
 package org.openjfx;
 
-import java.util.LinkedList;
-
 public class TreadSafeCreaturesArray {
     private Creature[] creatures;
     private boolean[] requestedCreatures;
-    private LinkedList<Thread>[] waitingThreads;
     public TreadSafeCreaturesArray(Creature[] creatures) {
         this.creatures = creatures;
         requestedCreatures=new boolean[creatures.length];
-        waitingThreads=new LinkedList[creatures.length];
         for(int i=0;i<requestedCreatures.length;i++){
             requestedCreatures[i]=false;
-            waitingThreads[i]=new LinkedList<Thread>();
         }
     }
 
-    public synchronized Creature request(int i,Thread t){
-        if(!requestedCreatures[i]){
-            requestedCreatures[i]=true;
-            return creatures[i];
-        }else{
-            waitingThreads[i].add(t);
-            try{
-                wait();
-            }catch(InterruptedException e){
-                e.printStackTrace();
-            }
-            requestedCreatures[i]=true;
-            return creatures[i];
+    public synchronized Creature request(int i)throws InterruptedException{
+        while(requestedCreatures[i]){
+            wait();
         }
+        requestedCreatures[i]=true;
+        return creatures[i];
     }
     public synchronized void release(int i){
-        if(!waitingThreads[i].isEmpty()){
-            waitingThreads[i].pop().notify();
-        }else{
-            requestedCreatures[i]=false;
-        }
+        requestedCreatures[i]=false;
+        notifyAll();
     }
 
     public Creature[] getCreatures() {
