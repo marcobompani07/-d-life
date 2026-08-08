893f7b2c0d1c88fa300e49b95d3dfe080ed77628
EdwardAiddo <aiddoedward@gmail.com>
2026-08-08 17:02:18 +0200

    created thread safe counter to manage creature index retrieval from CreatureActionHandler

---
+++ sample/src/main/java/org/openjfx/ThreadSafeCreatureUpdateCounter.java
@@ -0,0 +1,32 @@
+package org.openjfx;
+
+public class ThreadSafeCreatureUpdateCounter {
+	private int counter;
+	private final int max;
+	ThreadSafeCreatureArray creatureArray;
+
+	public ThreadSafeCreatureUpdateCounter(ThreadSafeCreatureArray creatureArray) {
+		this.creatureArray = creatureArray;
+		this.max = creatureArray.getLength();
+		this.counter = 0;
+	}
+
+	public synchronized int getNext() {
+		int current = counter;
+
+		while (creatureArray.getCreatureAtIndex(current) == null) {
+			current++;
+			if (current >= max) {
+				current = 0;
+			}
+		}
+
+		counter = current + 1;
+
+		if(counter >= max){
+			counter = 0;
+		}
+
+		return current;
+	}
+}
