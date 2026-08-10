3a89062fbde0b17df0760a9b2a407c029d68bdf0
marcobompani07 <marco.bompani.07@gmail.com>
2026-08-10 16:10:42 +0200

    timed cicling with creatures

--- sample/src/main/java/org/openjfx/ThreadSafeCreatureUpdateCounter.java
+++ sample/src/main/java/org/openjfx/ThreadSafeCreatureUpdateCounter.java
@@ -1,4 +1,4 @@
-package org.openjfx;
+/*package org.openjfx;
 
 public class ThreadSafeCreatureUpdateCounter {
 	private int counter;
@@ -26,6 +26,47 @@ public class ThreadSafeCreatureUpdateCounter {
 
 		if(counter >= max){
 			counter = 0;
+		}
+
+		return current;
+	}
+}*/
+
+
+package org.openjfx;
+
+public class ThreadSafeCreatureUpdateCounter {
+	private int counter;
+	private final int max;
+	private ThreadSafeCreaturesArray creatureArray;
+	private long startTime;
+
+	public ThreadSafeCreatureUpdateCounter(ThreadSafeCreaturesArray creatureArray) {
+		this.creatureArray = creatureArray;
+		this.max = creatureArray.getLength();
+		this.counter = 0;
+		startTime=System.currentTimeMillis();
+	}
+
+	public synchronized int getNext() throws InterruptedException {
+		int current = counter;
+
+		while (creatureArray.getCreatures()[current] == null) {
+			current++;
+
+			if (current >= max) {
+				current = 0;
+			}
+		}
+
+		counter = current + 1;
+
+		if(counter >= max){
+			if ((System.currentTimeMillis()-startTime)<=10){
+				Thread.sleep(10-(System.currentTimeMillis()-startTime));
+			}
+			startTime=System.currentTimeMillis();
+			counter=0;
 		}
 
 		return current;
