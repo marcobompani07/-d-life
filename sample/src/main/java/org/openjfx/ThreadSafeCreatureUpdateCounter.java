8190dca6b897f2820a333bdeef6d4de5462a5f2f
EdwardAiddo <aiddoedward@gmail.com>
2026-08-13 17:12:12 +0200

    fixed useless iteration in getNext

--- sample/src/main/java/org/openjfx/ThreadSafeCreatureUpdateCounter.java
+++ sample/src/main/java/org/openjfx/ThreadSafeCreatureUpdateCounter.java
@@ -18,14 +18,6 @@ public class ThreadSafeCreatureUpdateCounter {
 	public synchronized int getNext() throws InterruptedException {
 		int current = counter;
 
-		while (creatureArray.getCreatures()[current] == null) {
-			current++;
-
-			if (current >= max) {
-				current = 0;
-			}
-		}
-
 		counter = current + 1;
 
 		if(counter >= max){
