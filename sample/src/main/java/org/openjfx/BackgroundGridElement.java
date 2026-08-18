79959892d5f71c64e296b9a677e95028eb821081
EdwardAiddo <aiddoedward@gmail.com>
2026-08-18 18:29:49 +0200

    added creatureId to the grid and made the methods synchronized

--- sample/src/main/java/org/openjfx/BackgroundGridElement.java
+++ sample/src/main/java/org/openjfx/BackgroundGridElement.java
@@ -2,16 +2,26 @@ package org.openjfx;
 
 public class BackgroundGridElement {
     private int foodId;
+	private int creatureId;
 
     public BackgroundGridElement(){
-        foodId=-1;
+        foodId = -1;
+		creatureId = -1;
     }
 
-    public int getFood(){
+    public synchronized int getFood(){
         return foodId;
     }
 	
-    public void setFood(int foodId){
+    public synchronized void setFood(int foodId){
         this.foodId=foodId;
     }
+
+	public synchronized int getCreature(){
+		return creatureId;
+	}
+
+	public synchronized void setCreature(int creatureId){
+		this.creatureId = creatureId;
+	}
 }
